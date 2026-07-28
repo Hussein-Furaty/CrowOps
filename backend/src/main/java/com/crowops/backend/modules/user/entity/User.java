01fb7ecdd0ede69ab53b36db7b0d1ecb399c485f
Hussein.A.Habeeb <hussein.a.habeeb.sec@gmail.com>
2026-07-28 16:31:51 +0300

    Add User entity and repository

---
+++ backend/src/main/java/com/crowops/backend/modules/user/entity/User.java
@@ -0,0 +1,38 @@
+package com.crowops.backend.modules.user.entity;
+
+import com.crowops.backend.shared.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false, length = 100)
+    private String firstName;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    @Column(nullable = false)
+    private boolean locked = false;
+}
